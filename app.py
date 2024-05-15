ff74de07cf617dfc3ea10704df70e52ebe028943
gracieux-dev <chacoungracieux@gmail.com>
2024-05-15 11:07:29 +0200

    streamlit design

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
 # streamlit app.py
-
 import streamlit as st
-def main():
-   
-    st.write("""
-        # AI Verse
-        Your *tourism* and *cultural preservation* App
-    """)
-    st.sidebar.title("Side Bar title")
+import streamlit_shadcn_ui as ui
+from streamlit_extras.stylable_container import stylable_container
+from streamlit_option_menu import option_menu
 
-   
+st.set_page_config(page_title="LANGUATOUR AI", page_icon="", layout="wide")
 
-if __name__ == "__main__":
-    main()
+st.markdown("""
+            <style>
+                
+            </style>
+
+            """,
+            unsafe_allow_html=True)
+
+
+value = ui.tabs(options=['DISCOVERY', 'TRANSLATION', 'LEARN'], default_value='DISCOVERY', key="kanaries")
+
+with ui.card(key="image"):
+    if value == "DISCOVERY":
+        with stylable_container(
+                key="card2",
+                css_styles="""
+
+                """    
+            ):
+            with ui.card(key="card2"):
+                
+                ui.element("button", key="card2_btn", text="Nest Submmit", variant="outline")
+        with ui.card(key="card"):
+            ui.element("button", key="card2_btn", text="Nest Submmit", variant="outline")
+        with ui.card(key="card"):
+            ui.element("button", key="card2_btn", text="Nest Submmit", variant="outline")
+        with ui.card(key="card"):
+            ui.element("button", key="card2_btn", text="Nest Submmit", variant="outline")
+    elif value == "LEARN":
+        with ui.card(key="learn"):
+            col1, col2 = st.columns([1, 2], gap="medium")
+
+            with col1:
+                st.image('icon.png', width=50)
+
+            with col2:
+                with stylable_container(
+                    key="text",
+                    css_styles="""
+
+                    """
+                ):
+                    st.markdown("BEGINNERS")
+                    st.markdown("cotent for beginner")
+
+                
+
--- front-end/main.py
+++ front-end/main.py
@@ -1,8 +1,23 @@
 import flet as ft
+from flet import *
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+
+    #constant
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+    page.bgcolor = "#303030"
+
+
+    page.add(
+        Card(
+            width=360,
+            height=200,
+            elevation=7
+        )
+    )
+
 
 
 ft.app(main)
